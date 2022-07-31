84df274e74ed19916437dc2807505c42792ca100
decentration <ramseyajram@me.com>
2022-07-31 16:09:59 +0100

    add parachain-staking

--- runtime/src/lib.rs
+++ runtime/src/lib.rs
@@ -54,8 +54,8 @@ use weights::{BlockExecutionWeight, ExtrinsicBaseWeight, RocksDbWeight};
 use xcm::latest::prelude::BodyId;
 use xcm_executor::XcmExecutor;
 
-/// Import the template pallet.
-pub use pallet_template;
+/// Import parachain-staking.
+pub use parachain_staking;
 
 /// Alias to 512-bit hash when used in the context of a transaction signature on the chain.
 pub type Signature = MultiSignature;
@@ -455,7 +455,7 @@ impl pallet_collator_selection::Config for Runtime {
 }
 
 /// Configure the pallet template in pallets/template.
-impl pallet_template::Config for Runtime {
+impl parachain_staking::Config for Runtime {
 	type Event = Event;
 }
 
@@ -492,7 +492,7 @@ construct_runtime!(
 		DmpQueue: cumulus_pallet_dmp_queue::{Pallet, Call, Storage, Event<T>} = 33,
 
 		// Template
-		TemplatePallet: pallet_template::{Pallet, Call, Storage, Event<T>}  = 40,
+		ParachainStaking: parachain_staking::{Pallet, Call, Storage, Event<T>}  = 40,
 	}
 );
 
